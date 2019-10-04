8c3ee53a55f7b4035905f13113b80df0278d9aef
Matias Iudica <mattiudica@hotmail.com>
2019-10-04 16:23:21 -0300

    mas cambios de estilo a la vista investcreate

--- resources/views/balance/indexBalance.blade.php
+++ resources/views/balance/indexBalance.blade.php
@@ -10,8 +10,8 @@
         <div class="col-md-9">
             <div class="text-left">
                 <div class="form-group">
-                    <h4 style="color:blue">Cuenta 032-366474/9</h4>
-                    <h5 style="color:blue">Caja de Ahorro en Pesos<h5>
+                    <h4>Cuenta 032-366474/9</h4>
+                    <h5>Caja de Ahorro en Pesos<h5>
                     <h4 style="color:green"><strong>${{end($saldos)}}</strong></h4>
                 </div>       
             </div>
@@ -28,17 +28,17 @@
                 <tbody>
                     @foreach($balances as $each)
                         <tr>
-                            <th scope="col">{{ date('j/m/Y', strtotime($each->created_at)) }}</th>
-                            <th scope="col">{{ substr(($each->descripcion),0,12)}} {{strlen($each->descripcion) > 12 ? "..." : "" }}</th>
-                            <th scope="col">${{ $each->importe }}</th>
-                            <th scope="col" style="color:green">${{$saldos[$init--]}}</th>
+                            <td><p>{{ substr(($each->descripcion),0,12)}} {{strlen($each->descripcion) > 12 ? "..." : "" }}</p></th>
+                            <td><p>{{ date('j/m/Y', strtotime($each->created_at)) }}</p></th>
+                            <td><p>${{ $each->importe }}</p></th>
+                            <td><p style="color:green;font-weight:bold">${{$saldos[$init--]}}</p></th>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
             <br>
             <div class="text-center">
-               "{ !! $balances->links(); !! }""
+              "{ !! $balances->links(); !! }"
             </div>
             <br>
             <div class="text-right">
--- resources/views/invest/createInvest.blade.php
+++ resources/views/invest/createInvest.blade.php
@@ -11,23 +11,36 @@
         <table class="table table-borderless table-striped" id="invest-table">
             <thead>
                 <tr>
-                    <th scope="col"><p class="lead">Empresa</p></th>
+                    <th scope="col" class="float-left"><p class="lead">Empresa</p></th>
                     <th scope="col"><p class="lead">Acciones</p></th>
                     <th scope="col"><p class="lead">Valor</p> </th>
-                    <th scope="col" class="float-right"><p class="lead">Gestion</p> </th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td><strong>Arcor</strong></td>
-                    <td><strong>500</strong></td>
-                    <td><strong>25</strong></td>
-                    <td>
-                        <div class="form-group float-right">
-                            <button type="button" class="btn btn-sm btn-secondary">Vender</button>
-                            <button type="button" class="btn btn-sm btn-info">Comprar</button>
-                        </div>
+                    <td class="text-left">
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="company" id="company" value="option1" aria-label="...">
+                            <label class="form-check-label" for="company" style="padding-left:5px;">Arcor</label>
+                        </div> 
                     </td>
+                    <td><p style="color:blue;font-weight:bold">500</p></td>
+                    <td><p style="color:green;font-weight:bold">25</p></td>
+                </tr>
+                <tr>
+                    <td class="text-left">
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="company" id="company" value="option1" aria-label="...">
+                            <label class="form-check-label" for="company" style="padding-left:5px;">La Serenisima</label>
+                        </div> 
+                    </td>
+                    <td><p style="color:blue;font-weight:bold">200</p></td>
+                    <td><p style="color:green;font-weight:bold">70</p></td>
+                </tr>
+                <tr>
+                    <td><p class="lead">Trader</p></td>
+                    <td><button type="button" class="btn btn-sm btn-primary">Vender</button></td>
+                    <td><button type="button" class="btn btn-sm btn-success">Comprar</button></td>
                 </tr>
             </tbody>
         </table>
